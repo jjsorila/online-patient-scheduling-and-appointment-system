bcc7c6362676e29bd7aead38c7a2b32d26651fdc
JJS <jjsorila@gmail.com>
2022-09-25 21:50:53 +0800

    updated login and home

---
+++ assets/js/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function(e) {
+    $("#to-reg-btn").click(function(e) {
+        $("form").toggleClass("slide")
+    })
+
+    $("#to-login-btn").click(function(e) {
+        $("form").toggleClass("slide")
+    })
+})
