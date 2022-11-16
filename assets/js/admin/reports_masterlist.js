9aa5348290f10e7a15660bd93389abc376ecd999
JJS <jjsorila@gmail.com>
2022-11-17 00:04:34 +0800

    changes in reports masterlist

--- assets/js/admin/reports_masterlist.js
+++ assets/js/admin/reports_masterlist.js
@@ -18,7 +18,8 @@ $(document).ready(function(e) {
             }
         ],
         columnDefs: [
-            { className: "dt-center", targets: "_all" }
+            { className: "dt-center", targets: "_all" },
+            { className: "text-break w-25", targets: 1 }
         ],
         lengthMenu: [[10, 20, 30, 50, -1], [10, 20, 30, 50, "All"]],
         order: [[0, 'asc']],
@@ -33,8 +34,7 @@ $(document).ready(function(e) {
             },
             {
                 data: "address",
-                orderable: false,
-                render: (addr) => (`${addr.slice(0, 15)}...`)
+                orderable: false
             },
             {
                 data: "contact",
