ef741ffdabe08d510f6513a649cf58104f1638fc
JJS <jjsorila@gmail.com>
2023-08-14 18:49:01 +0800

    no-caching test

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -3,10 +3,13 @@ const jwt = require("jsonwebtoken")
 
 exports.protected = (req, res, next) => {
     if (!req.session.admin) return res.redirect('/login')
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
     return next()
 }
 
 exports.onlyAdmin = (req, res, next) => {
-    if(req.session.admin.specialty != "admin") return res.redirect('/admin/dashboard')
+    if (req.session.admin.specialty != "admin") return res.redirect('/admin/dashboard')
     return next()
 }
--- middlewares/client.js
+++ middlewares/client.js
@@ -3,6 +3,9 @@ const jwt = require("jsonwebtoken")
 
 exports.protected = (req, res, next) => {
     if (!req.session.user) return res.redirect('/login')
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
     return next()
 }
 
